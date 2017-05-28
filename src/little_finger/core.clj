ca9731822a921371aed17262a950a27540771d2a
avi <hi@avi.im>
2017-05-28 22:27:34 +0530

    Add index and statusHandler

--- src/little_finger/core.clj
+++ src/little_finger/core.clj
@@ -1,7 +1,24 @@
 (ns little-finger.core
-  (:gen-class))
+  (:require [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]
+            [compojure.handler :refer [site]]
+            [ring.adapter.jetty :as jetty]))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn index []
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "The Little Finger is up"})
+
+(defn statusHandler []
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Kill the app"})
+
+(defroutes app
+  (GET "/" []
+       (index))
+  (GET "/status" []
+       (statusHandler)))
+
+(defn -main [& [port]]
+  (let [port (Integer. 5000)]
+    (jetty/run-jetty (site #'app) {:port port :join? false})))
